2b3763e4a441a0118b71d48e136af42496fa703c
SuJin <1127sujink@naver.com>
2023-02-03 21:18:59 +0900

    TIL : crawling

---
+++ crawler/crawl2.js
@@ -0,0 +1,52 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+const url = '';
+
+async function main() {
+  const resp = await axios.get(url + "/Contest/CateField.html");
+
+  const $ = cheerio.load(resp.data); 
+  const elements = $(".contest-banner-list li"); 
+//   console.log(elements);
+  const title = [];
+  elements.each((idx, el) => {
+    console.log($(el).text());
+    if ($(el).text() !== "") {
+      title.push($(el).text());
+    }
+  });
+
+  const hrefarr = [];
+
+  $(".contest-banner-list")
+    .find("li")
+    .each(function (index, ele) {
+      var ahref = $(this).find("a").eq(0).attr("href");
+      console.log("href",  ahref);
+      if (ahref !== undefined) {
+        hrefarr.push(ahref);
+      }
+      console.log('arr', hrefarr)
+      console.log("***");
+    });
+
+  for (let i = 0; i < hrefarr.length; i++) {
+    if (hrefarr[i][1] === "C") {
+      console.log("title: ", title[i]);
+      const hrefresp = await axios.get(
+        url + hrefarr[i]
+      );
+      hrefContents(hrefresp);
+    } 
+  }
+}
+
+main();
+
+async function hrefContents(resp) {
+  const $ = cheerio.load(resp.data);
+  const elements = $(".contest-detail div"); 
+  elements.each((idx, el) => {
+    console.log($(el).text());
+  });
+}
